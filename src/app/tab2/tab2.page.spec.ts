95ce7a0657ccd563052c61a016ab1fb6890722be
Jacob A Johnkoski <arjohnkoski@gmail.com>
2024-03-23 11:59:52 -0500

    rename tab 2 header and button

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Tab2Page } from './tab2.page';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
+  let tabTwoHeader: DebugElement;
 
   beforeEach(async () => {
     fixture = TestBed.createComponent(Tab2Page);
+    tabTwoHeader = fixture.debugElement.query(By.css('ion-header'));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have `Cats` as the page title', () => {
+    const tabTwoTitle = tabTwoHeader.query(By.css('ion-title'));
+    expect(tabTwoTitle.nativeElement.innerText).toBe('Cats');
   });
 });
--- src/app/tabs/tabs.page.spec.ts
+++ src/app/tabs/tabs.page.spec.ts
@@ -1,11 +1,15 @@
+import { IonLabel } from '@ionic/angular/standalone';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TabsPage } from './tabs.page';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('TabsPage', () => {
   let component: TabsPage;
   let fixture: ComponentFixture<TabsPage>;
+  let tabTwo: DebugElement;
 
   beforeEach(async () => {
     TestBed.overrideComponent(TabsPage, {
@@ -17,11 +21,17 @@ describe('TabsPage', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TabsPage);
+    tabTwo = fixture.debugElement.query(By.css('ion-tab-button[tab="tab2"]'));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have `Cats` as the tab 2 label', () => {
+    const tabTwoLabel = tabTwo.query(By.css('ion-label'));
+    expect(tabTwoLabel.nativeElement.innerText).toBe('Cats');
   });
 });
