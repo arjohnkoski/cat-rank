7700f000db5c99a211add82040d6eb9d58ae95dd
Jacob A Johnkoski <arjohnkoski@gmail.com>
2024-03-22 23:06:35 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cat.rank',
+  appName: 'cat-rank',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
