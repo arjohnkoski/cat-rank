167c2932f15308c23b0b3b6fb5647d2ba54728e5
Jacob A Johnkoski <arjohnkoski@gmail.com>
2024-03-22 23:24:18 -0500

    add packages

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 if (environment.production) {
   enableProdMode();
 }
+
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, {
   providers: [
