277106803dbae122a5bd565f2e2529f9f187216b
Jacob A Johnkoski <arjohnkoski@gmail.com>
2024-03-29 09:39:17 -0500

    Add camera fab and spec refactor

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,17 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Tab2Page } from './tab2.page';
-import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { Tab2Page } from './tab2.page';
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
-  let tabTwoHeader: DebugElement;
+  let header: DebugElement;
+
+  const icons: any = {
+    camera: 'camera'
+  };
 
   beforeEach(async () => {
     fixture = TestBed.createComponent(Tab2Page);
-    tabTwoHeader = fixture.debugElement.query(By.css('ion-header'));
+    header = fixture.debugElement.query(By.css('ion-header'));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,8 +23,29 @@ describe('Tab2Page', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have the correct icons', () => {
+      expect(Object.keys(component.icons).length)
+        .toEqual(Object.keys(icons).length);
+      Object.values(icons).forEach(icon => {
+        expect(component.icons[`${icon}`]).toBeTruthy();
+      });
+    });
+
   it('should have `cats` as the page title', () => {
-    const tabTwoTitle = tabTwoHeader.query(By.css('ion-title'));
-    expect(tabTwoTitle.nativeElement.innerText).toBe('cats');
+    const title = header.query(By.css('ion-title'));
+    expect(title.nativeElement.innerText).toBe('cats');
+  });
+
+  it('should have a camera button', () => {
+    const fab = fixture.debugElement.query(By.css('ion-fab'));
+    expect(fab).toBeTruthy();
+    expect(fab.nativeElement.vertical).toBe('bottom');
+    expect(fab.nativeElement.horizontal).toBe('center');
+    expect(fab.nativeElement.slot).toBe('fixed');
+    const fabButton = fab.query(By.css('ion-fab-button'));
+    expect(fabButton).toBeTruthy();
+    const fabIcon = fabButton.query(By.css('ion-icon'));
+    expect(fabIcon).toBeTruthy();
+    expect(fabIcon.nativeElement.name).toBe(icons.camera);
   });
 });
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,33 @@
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonContent,
+         IonFab,
+         IonFabButton,
+         IonHeader,
+         IonIcon,
+         IonTitle,
+         IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [ ExploreContainerComponent,
+             IonContent,
+             IonFab,
+             IonFabButton,
+             IonHeader,
+             IonIcon,
+             IonTitle,
+             IonToolbar ]
 })
 export class Tab2Page {
+  public icons: any = { camera };
 
-  constructor() {}
-
+  constructor() {
+    addIcons(this.icons);
+  }
 }
